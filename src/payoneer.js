9c4abbf92630ff2da3c4fe15f98ec839ec180b69
Abel M. Osorio <abel.m.osorio@gmail.com>
2017-03-03 20:02:20 -0300

    Some code changes in order to make it more ES6 compilant. Added a bit of documentation using JSDoc syntax.

--- src/payoneer.js
+++ src/payoneer.js
@@ -1,53 +1,99 @@
-function Payoneer(config) {
-  const pyrequest = require('./pyrequest.js')(config);
+/**
+ * Payoneer API Client.
+ *
+ * @module Payoneer
+ *
+ * @todo Complete the documentation.
+ */
 
-  return {
-    getVersion: function() {
-      return new Promise(function(success, reject) {
-        pyrequest.post('GetVersion').then(function(data) {
-          success(data);
-        }).catch(function(error) {
-          reject(error);
-        });
-      });
-    },
+const PYRequest = require('./pyrequest');
 
-    getPaymentStatus: function(p4, p5) {
-      return new Promise(function(success, reject) {
-        pyrequest.post(
-          'GetPaymentStatus',
-          { p4: p4, p5: p5 }
-        ).then(function(data) {
-          success(data);
-        }).catch(function(error) {
-          reject(error);
-        });
-      });
-    },
+/**
+ * Payoneer API client.
+ */
+class Payoneer {
+  /**
+   * @constructor
+   */
+  constructor(config) {
+    this.pyrequest = new PYRequest(config);
+  }
 
-    getPayeeDetails: function(p4) {
-      return new Promise(function(success, reject) {
-        pyrequest.post('GetPayeeDetails', { p4: p4 }).then(function(data) {
-          success(data);
-        }).catch(function(error) {
-          reject(error);
-        });
+  /**
+   * Common Payoneer post.
+   *
+   * @param {string} mname - The method name (eg 'GetVersion').
+   * @param {Object} parameters - The method parameters.
+   *
+   * @returns {Promise} A new Promise. If there is no errors, the result
+   *                    will be a JavaScript object.
+   */
+  pyPost(mname, parameters) {
+    return new Promise((success, reject) => {
+      this.pyrequest.post(mname, parameters).then(function(data) {
+        success(data);
+      }).catch(function(error) {
+        reject(error);
       });
-    },
+    });
+  }
 
-    sendPayment: function(p4, p5, p6, p7, p8) {
-      return new Promise(function(success, reject) {
-        pyrequest.post(
-          'PerformPayoutPayment',
-          { p4: p4, p5: p5, p6: p6, p7: p7, p8: p8 }
-        ).then(function(data) {
-          success(data);
-        }).catch(function(error) {
-          reject(error);
-        });
-      });
-    }
-  };
-};
+  /**
+   * Return the API version.
+   *
+   * @example
+   * const payoneer = new Payoneer({
+   *   uri: process.env.URI,
+   *   username: process.env.PARTNER_USERNAME,
+   *   apiPassword: process.env.PARTNER_API_PASSWORD,
+   * });
+   *
+   * let response = payoneer.getVersion();
+   *
+   * alert(`Payoneer API Version: ${response.PayoneerResponse.Version}`);
+   *
+   * @returns {Object} Representing API version.
+   */
+  getVersion() {
+    return this.pyPost('GetVersion');
+  }
+
+  /**
+   * Report the status of a payment that was previously sent to the Payoneer
+   * system.
+   *
+   * @todo Complete the documentation.
+   */
+  getPaymentStatus(p4, p5) {
+    return this.pyPost('GetPaymentStatus', { p4: p4, p5: p5 });
+  }
+
+  /**
+   * Return details about the requested payee and its status at Payoneer.
+   *
+   * @todo Complete the documentation.
+   */
+  getPayeeDetails(p4) {
+    return this.pyPost('GetPayeeDetails', { p4: p4 });
+  }
+
+  /**
+   * Submit a payment request to Payoneer's system.
+   *
+   * Prior to issuing payment instructions, the client's account balance in
+   * Payoneer's system must be credited to cover the instructions requested.
+   * Payout instruction submission will fail if the client's account balance
+   * does not have sufficient funds to perform the payment instructions. In
+   * addition, the payee ID must be active.
+   *
+   * @todo Complete the documentation.
+   */
+  sendPayment(p4, p5, p6, p7, p8) {
+    return this.pyPost(
+      'PerformPayoutPayment',
+      { p4: p4, p5: p5, p6: p6, p7: p7, p8: p8 }
+    );
+  }
+}
 
 module.exports = Payoneer;
--- src/pyrequest.js
+++ src/pyrequest.js
@@ -1,41 +1,58 @@
-var rp = require('request-promise');
-var parseXML = require('xml2js').parseString;
+/**
+ * Payoneer Request handler.
+ *
+ * @module PYRequest
+ */
 
-var PYRequest = function(config) {
-  return {
-    post: function(mname, parameters) {
-      var credentials = {
-        p1: config.username,
-        p2: config.apiPassword,
-        p3: config.partnerId
-      };
+const rp = require('request-promise');
+const parseXML = require('xml2js').parseString;
 
-      var qs = Object.assign(credentials, { mname: mname }, parameters);
+/**
+ * PYRequest
+ */
+class PYRequest {
+  /**
+   * @constructor
+   *
+   * @param {Object} config - Common configuration. You must suplly the
+   *                          following parameters: username, apiPassword,
+   *                          partnerId.
+   */
+  constructor(config) {
+    this.config = config;
+    this.credentials = {
+      p1: this.config.username,
+      p2: this.config.apiPassword,
+      p3: this.config.partnerId
+    };
+  }
 
-      var options = {
-        uri: config.uri,
-        method: 'POST',
-        port: 443,
-        qs: qs
-      };
+  post(mname, parameters) {
+    let qs = Object.assign(this.credentials, { mname: mname }, parameters);
 
-      var response = rp(options);
+    let options = {
+      uri: this.config.uri,
+      method: 'POST',
+      port: 443,
+      qs: qs
+    };
 
-      return new Promise(function(success, reject) {
-        response.then(function(data) {
-          parseXML(data, function(error, result) {
-            if (error) {
-              reject(new Error(error));
-            }
-            success(result);
-          });
-        })
-        .catch(function(error) {
-          reject(error);
+    let response = rp(options);
+
+    return new Promise(function(success, reject) {
+      response.then(function(data) {
+        parseXML(data, function(error, result) {
+          if (error) {
+            reject(new Error(error));
+          }
+          success(result);
         });
+      })
+      .catch(function(error) {
+        reject(error);
       });
-    }
-  };
-};
+    });
+  }
+}
 
 module.exports = PYRequest;
