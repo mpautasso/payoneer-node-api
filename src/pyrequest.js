b7ea5233636b8561d6faa1a47955c2aef909421b
Abel M. Osorio <abel.m.osorio@gmail.com>
2017-03-03 19:01:21 -0300

    Some code adjustments to avoid Eslint complaints.

--- src/pyrequest.js
+++ src/pyrequest.js
@@ -1,16 +1,16 @@
 var rp = require('request-promise');
 var parseXML = require('xml2js').parseString;
 
-var PYRequest = function(config){
+var PYRequest = function(config) {
   return {
-    post: function(mname, parameters){
+    post: function(mname, parameters) {
       var credentials = {
         p1: config.username,
         p2: config.apiPassword,
         p3: config.partnerId
       };
 
-      var qs = Object.assign(credentials, { mname: mname }, parameters)
+      var qs = Object.assign(credentials, { mname: mname }, parameters);
 
       var options = {
         uri: config.uri,
@@ -19,11 +19,11 @@ var PYRequest = function(config){
         qs: qs
       };
 
-      var response = rp(options)
+      var response = rp(options);
 
       return new Promise(function(success, reject) {
         response.then(function(data) {
-          parseXML(data, function (error, result) {
+          parseXML(data, function(error, result) {
             if (error) {
               reject(new Error(error));
             }
@@ -33,10 +33,9 @@ var PYRequest = function(config){
         .catch(function(error) {
           reject(error);
         });
-
       });
     }
-  }
-}
+  };
+};
 
-module.exports = PYRequest
+module.exports = PYRequest;
