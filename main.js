dcea91d2c45e946fa540acda718335325005d061
Mauricio Pautasso <mauricio.pautasso@gmail.com>
2017-03-05 16:41:45 -0300

    Adjust main.js file to avoid Eslint complaints.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-const util = require('util')
 
-Payoneer = require('./src/payoneer');
+const util = require('util');
+const Payoneer = require('./src/payoneer');
 
-payoneer = new Payoneer({
+const payoneer = new Payoneer({
   uri: process.env.URI,
   username: process.env.PARTNER_USERNAME,
   apiPassword: process.env.PARTNER_API_PASSWORD,
@@ -11,89 +11,89 @@ payoneer = new Payoneer({
 });
 
 // Return the status of the API
-// payoneer.echo().then(function(data){
+// payoneer.echo().then(function(data) {
 //   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
-// }).catch(function(error){
-//   console.log(error.message)
+// }).catch(function(error) {
+//   console.log(error.message);
 // });
 
 // Return the API version
-// payoneer.getVersion().then(function(data){
+// payoneer.getVersion().then(function(data) {
 //   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
-// }).catch(function(error){
-//   console.log(error.message)
+// }).catch(function(error) {
+//   console.log(error.message);
 // });
 
 // Submit a payment request to Payoneer's system
-// payoneer.sendPayment('AGILITYFEAT100069280-PAY', 9, 10, 20, 'Created from the wrapper', null, null, 'USD').then(function(data){
+// payoneer.sendPayment('AGILITYFEAT100069280-PAY', 9, 10, 20, 'Created from the wrapper', null, null, 'USD').then(function(data) {
 //   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
-// }).catch(function(error){
-//   console.log(error.message)
+// }).catch(function(error) {
+//   console.log(error.message);
 // });
 
 // Report the status of a payment that was previously sent to Payoneer system
-// payoneer.getPaymentStatus(10, 7).then(function(data){
+// payoneer.getPaymentStatus(10, 7).then(function(data) {
 //   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
-// }).catch(function(error){
-//   console.log(error.message)
+// }).catch(function(error) {
+//   console.log(error.message);
 // });
 
 // Return the client's account balance
-// payoneer.getAccountDetails().then(function(data){
+// payoneer.getAccountDetails().then(function(data) {
 //   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
-// }).catch(function(error){
-//   console.log(error.message)
+// }).catch(function(error) {
+//   console.log(error.message);
 // });
 
 // Return details about the requested payee and its status at Payoneer
-// payoneer.getPayeeDetails(10).then(function(data){
+// payoneer.getPayeeDetails(10).then(function(data) {
 //   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
-// }).catch(function(error){
-//   console.log(error.message)
+// }).catch(function(error) {
+//   console.log(error.message);
 // });
 
 // Change the ID of an existing payee (old) to a new one
-// payoneer.changePayeeId(10, 13).then(function(data){
+// payoneer.changePayeeId(10, 13).then(function(data) {
 //   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
-// }).catch(function(error){
-//   console.log(error.message)
+// }).catch(function(error) {
+//   console.log(error.message);
 // });
 
 // Return all payments made to payees, grouped by a payment method
-payoneer.getPayeesReport(0, '02/01/2016', '02/28/2016').then(function(data){
-  console.log('Response:');
-  console.log(util.inspect(data, { depth: null, colors: true }));
-}).catch(function(error){
-  console.log(error.message)
-});
-
-// Return all payments made to a specified payee
-// payoneer.getSinglePayeeReport(10).then(function(data){
+// payoneer.getPayeesReport(0, '02/01/2016', '02/28/2016').then(function(data) {
 //   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
-// }).catch(function(error){
-//   console.log(error.message)
+// }).catch(function(error) {
+//   console.log(error.message);
+// });
+
+// Return all payments made to a specified payee
+// payoneer.getSinglePayeeReport(10).then(function(data) {
+//   console.log('Response:');
+//   console.log(util.inspect(data, { depth: null, colors: true }));
+// }).catch(function(error) {
+//   console.log(error.message);
 // });
 
 // Return a list of all payments that are unclaimed in XML format
-// payoneer.getUnclaimedPaymentsXml().then(function(data){
+// payoneer.getUnclaimedPaymentsXml().then(function(data) {
 //   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
-// }).catch(function(error){
-//   console.log(error.message)
+// }).catch(function(error) {
+//   console.log(error.message);
 // });
 
 // Cancel a payment that has not been processed and loaded to an account
-// payoneer.cancelPayment(8).then(function(data){
+// payoneer.cancelPayment(8).then(function(data) {
 //   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
-// }).catch(function(error){
-//   console.log(error.message)
+// }).catch(function(error) {
+//   console.log(error.message);
 // });
