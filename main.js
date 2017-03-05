2baca4c3f11ec5968efa3316822f031ed9f42703
Mauricio Pautasso <mauricio.pautasso@gmail.com>
2017-03-05 16:03:08 -0300

    Update main.js file

--- main.js
+++ main.js
@@ -10,8 +10,25 @@ payoneer = new Payoneer({
   partnerId: process.env.PARTNER_ID
 });
 
+// Return the status of the API
+// payoneer.echo().then(function(data){
+//   console.log('Response:');
+//   console.log(util.inspect(data, { depth: null, colors: true }));
+// }).catch(function(error){
+//   console.log(error.message)
+// });
+
 // Return the API version
 // payoneer.getVersion().then(function(data){
+//   console.log('Response:');
+//   console.log(util.inspect(data, { depth: null, colors: true }));
+// }).catch(function(error){
+//   console.log(error.message)
+// });
+
+// Submit a payment request to Payoneer's system
+// payoneer.sendPayment('AGILITYFEAT100069280-PAY', 9, 10, 20, 'Created from the wrapper', null, null, 'USD').then(function(data){
+//   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
 // }).catch(function(error){
 //   console.log(error.message)
@@ -19,6 +36,15 @@ payoneer = new Payoneer({
 
 // Report the status of a payment that was previously sent to Payoneer system
 // payoneer.getPaymentStatus(10, 7).then(function(data){
+//   console.log('Response:');
+//   console.log(util.inspect(data, { depth: null, colors: true }));
+// }).catch(function(error){
+//   console.log(error.message)
+// });
+
+// Return the client's account balance
+// payoneer.getAccountDetails().then(function(data){
+//   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
 // }).catch(function(error){
 //   console.log(error.message)
@@ -26,21 +52,48 @@ payoneer = new Payoneer({
 
 // Return details about the requested payee and its status at Payoneer
 // payoneer.getPayeeDetails(10).then(function(data){
+//   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
 // }).catch(function(error){
 //   console.log(error.message)
 // });
 
-// Submit a payment request to Payoneer's system
-// payoneer.sendPayment('AGILITYFEAT100069280-PAY', 8, 10, 20, 'Created from the wrapper').then(function(data){
+// Change the ID of an existing payee (old) to a new one
+// payoneer.changePayeeId(10, 13).then(function(data){
+//   console.log('Response:');
 //   console.log(util.inspect(data, { depth: null, colors: true }));
 // }).catch(function(error){
 //   console.log(error.message)
 // });
 
-// Report the status of a payment that was previously sent to the Payoneer system
-payoneer.getPaymentStatus(10, 8).then(function(data){
+// Return all payments made to payees, grouped by a payment method
+payoneer.getPayeesReport(0, '02/01/2016', '02/28/2016').then(function(data){
+  console.log('Response:');
   console.log(util.inspect(data, { depth: null, colors: true }));
 }).catch(function(error){
   console.log(error.message)
 });
+
+// Return all payments made to a specified payee
+// payoneer.getSinglePayeeReport(10).then(function(data){
+//   console.log('Response:');
+//   console.log(util.inspect(data, { depth: null, colors: true }));
+// }).catch(function(error){
+//   console.log(error.message)
+// });
+
+// Return a list of all payments that are unclaimed in XML format
+// payoneer.getUnclaimedPaymentsXml().then(function(data){
+//   console.log('Response:');
+//   console.log(util.inspect(data, { depth: null, colors: true }));
+// }).catch(function(error){
+//   console.log(error.message)
+// });
+
+// Cancel a payment that has not been processed and loaded to an account
+// payoneer.cancelPayment(8).then(function(data){
+//   console.log('Response:');
+//   console.log(util.inspect(data, { depth: null, colors: true }));
+// }).catch(function(error){
+//   console.log(error.message)
+// });
